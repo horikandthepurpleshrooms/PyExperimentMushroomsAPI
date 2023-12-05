d920f7397748160478e0791e9760fbdf20c72cc6
Horik <h22ea@icloud.com>
2023-12-05 16:08:30 +0200

    create app py file

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+from flask import Flask
+from models import db
+from routes import mushrooms_bp
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mushrooms.db'  # Use SQLite database
+
+# Register models and blueprints
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+app.register_blueprint(mushrooms_bp, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(debug=True)
