c7baeb03fa6af9b4a8efaaaf3b701310d1acd971
Horik <h22ea@icloud.com>
2023-12-05 18:30:52 +0200

    added documentation

--- routes/mushrooms.py
+++ routes/mushrooms.py
@@ -1,76 +1,93 @@
+# routes/mushrooms.py
 from flask import Blueprint, jsonify, request
+from flask_restx import Resource, Api, fields, Namespace
 from models import db, Mushroom
 from .validation_helpers import validate_mushroom_data
 
 mushrooms_bp = Blueprint('mushrooms', __name__)
+api = Api(mushrooms_bp, version='1.0', title='Mushrooms API', description='CRUD operations for mushrooms')
 
-# Get all mushrooms
-@mushrooms_bp.route('/mushrooms', methods=['GET'])
-def get_mushrooms():
-    try:
-        mushrooms = Mushroom.query.all()
-        mushroom_list = [{'id': m.id, 'name': m.name, 'description': m.description, 'edible': m.edible} for m in mushrooms]
-        return jsonify({'mushrooms': mushroom_list})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+namespace = Namespace('mushrooms', description='Mushroom operations')
+api.add_namespace(namespace)
 
-# Get a specific mushroom
-@mushrooms_bp.route('/mushrooms/<int:mushroom_id>', methods=['GET'])
-def get_mushroom(mushroom_id):
-    try:
-        mushroom = Mushroom.query.get_or_404(mushroom_id)
-        return jsonify({'mushroom': {'id': mushroom.id, 'name': mushroom.name, 'description': mushroom.description, 'edible': mushroom.edible}})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+# Define a data model for the Mushroom resource
+mushroom_model = api.model('Mushroom', {
+    'id': fields.Integer(readonly=True, description='The mushroom identifier'),
+    'name': fields.String(required=True, description='The mushroom name'),
+    'description': fields.String(description='The mushroom description'),
+    'edible': fields.Boolean(description='Is the mushroom edible?')
+})
 
-# Create a new mushroom
-@mushrooms_bp.route('/mushrooms', methods=['POST'])
-def create_mushroom():
-    try:
-        data = request.json
+@namespace.route('/mushrooms')
+class MushroomsResource(Resource):
+    @namespace.marshal_with(mushroom_model, envelope='mushrooms')
+    def get(self):
+        """Get all mushrooms"""
+        try:
+            mushrooms = Mushroom.query.all()
+            return mushrooms
+        except Exception as e:
+            api.abort(500, 'Internal Server Error', error=str(e))
 
-        # Input validation
-        is_valid, validation_error = validate_mushroom_data(data)
-        if not is_valid:
-            return jsonify(validation_error), 400
+    @namespace.expect(mushroom_model)
+    @namespace.marshal_with(mushroom_model, envelope='mushroom')
+    def post(self):
+        """Create a new mushroom"""
+        try:
+            data = request.json
+            is_valid, validation_error = validate_mushroom_data(data)
+            if not is_valid:
+                api.abort(400, 'Validation error', errors=validation_error)
 
-        new_mushroom = Mushroom(name=data['name'], description=data.get('description', ''), edible=data.get('edible', False))
-        db.session.add(new_mushroom)
-        db.session.commit()
-        return jsonify({'mushroom': {'id': new_mushroom.id, 'name': new_mushroom.name, 'description': new_mushroom.description, 'edible': new_mushroom.edible}}), 201
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
+            new_mushroom = Mushroom(name=data['name'], description=data.get('description', ''), edible=data.get('edible', False))
+            db.session.add(new_mushroom)
+            db.session.commit()
+            return new_mushroom, 201
+        except Exception as e:
+            db.session.rollback()
+            api.abort(500, 'Internal Server Error', error=str(e))
 
-# Update a mushroom
-@mushrooms_bp.route('/mushrooms/<int:mushroom_id>', methods=['PUT'])
-def update_mushroom(mushroom_id):
-    try:
-        mushroom = Mushroom.query.get_or_404(mushroom_id)
-        data = request.json
+@namespace.route('/mushrooms/<int:mushroom_id>')
+@namespace.response(404, 'Mushroom not found')
+@namespace.param('mushroom_id', 'The mushroom identifier')
+class MushroomResource(Resource):
+    @namespace.marshal_with(mushroom_model, envelope='mushroom')
+    def get(self, mushroom_id):
+        """Get a specific mushroom"""
+        try:
+            mushroom = Mushroom.query.get_or_404(mushroom_id)
+            return mushroom
+        except Exception as e:
+            api.abort(500, 'Internal Server Error', error=str(e))
 
-        # Input validation
-        is_valid, validation_error = validate_mushroom_data(data)
-        if not is_valid:
-            return jsonify(validation_error), 400
+    @namespace.expect(mushroom_model)
+    @namespace.marshal_with(mushroom_model, envelope='mushroom')
+    def put(self, mushroom_id):
+        """Update a mushroom"""
+        try:
+            mushroom = Mushroom.query.get_or_404(mushroom_id)
+            data = request.json
+            is_valid, validation_error = validate_mushroom_data(data)
+            if not is_valid:
+                api.abort(400, 'Validation error', errors=validation_error)
 
-        mushroom.name = data.get('name', mushroom.name)
-        mushroom.description = data.get('description', mushroom.description)
-        mushroom.edible = data.get('edible', mushroom.edible)
-        db.session.commit()
-        return jsonify({'mushroom': {'id': mushroom.id, 'name': mushroom.name, 'description': mushroom.description, 'edible': mushroom.edible}})
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
+            mushroom.name = data.get('name', mushroom.name)
+            mushroom.description = data.get('description', mushroom.description)
+            mushroom.edible = data.get('edible', mushroom.edible)
+            db.session.commit()
+            return mushroom
+        except Exception as e:
+            db.session.rollback()
+            api.abort(500, 'Internal Server Error', error=str(e))
 
-# Delete a mushroom
-@mushrooms_bp.route('/mushrooms/<int:mushroom_id>', methods=['DELETE'])
-def delete_mushroom(mushroom_id):
-    try:
-        mushroom = Mushroom.query.get_or_404(mushroom_id)
-        db.session.delete(mushroom)
-        db.session.commit()
-        return jsonify({'message': 'Mushroom deleted!'})
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
+    @namespace.response(204, 'Mushroom deleted')
+    def delete(self, mushroom_id):
+        """Delete a mushroom"""
+        try:
+            mushroom = Mushroom.query.get_or_404(mushroom_id)
+            db.session.delete(mushroom)
+            db.session.commit()
+            return '', 204
+        except Exception as e:
+            db.session.rollback()
+            api.abort(500, 'Internal Server Error', error=str(e))
